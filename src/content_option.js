2829480b7f144ee5cb885f0a60ce14458c4f7923
Jordan Young <jordanby@uci.edu>
2023-03-01 15:21:50 -0800

    trying to push src

---
+++ src/content_option.js
@@ -0,0 +1,195 @@
+import bonfirePic from './assets/images/bonfire.JPG'
+import coachellaGuys from './assets/images/coachellaGuys.jpg'
+import dyePic from './assets/images/dye.jpg'
+import peninsulaLauren from './assets/images/peninsulaLauren.JPG'
+import pumpRoof from './assets/images/pumpRoof.jpg'
+import soundersGame from './assets/images/soundersGame.JPG'
+import berlin from './assets/images/berlin.JPG'
+import thiryEighthGrad from './assets/images/38thGrad.jpg'
+import crssd from './assets/images/crssd.jpg'
+import diggleJTree from './assets/images/diggleJTree.JPG'
+import dodgersStadium from './assets/images/dodgersStadium.jpg'
+import tahoeSunrise from './assets/images/tahoeSunrise.JPG'
+import soloGrad from './assets/images/soloGrad.JPG'
+
+
+
+const logotext = "JORDAN";
+const meta = {
+    title: "Jordan Young",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Jordan Young",
+    animated: {
+        first: "I'm often coding",
+        second: "Or traveling",
+        third: "And I love music",
+    },
+    description: "Here's a little portal to my life. Hope you enjoy and feel free to connect",
+    your_img_url: soloGrad,
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I recently graduated from the University of California, Irvine, with a Bachelor's degree in Computer Science. Upon graduation I began full time at Amazon working as a Software Engineer after interning the Summer before.",
+};
+const worktimeline = [{
+        jobtitle: "Software Engineer",
+        where: "Amazon",
+        date: "2022",
+    },
+    {
+        jobtitle: "Software Engineer Intern",
+        where: "Amazon",
+        date: "2021",
+    },
+    {
+        jobtitle: "Full Stack Engineer",
+        where: "Commit the Change, UCI",
+        date: "2020",
+    },
+    {
+        jobtitle: "Research Software Engineer",
+        where: "Beckman Laser Institute",
+        date: "2019",
+    }
+];
+
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "TypeScript",
+        value: 90,
+    },
+    {
+        name: "AWS",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: bonfirePic,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: coachellaGuys,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: dyePic,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: pumpRoof,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: peninsulaLauren,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: soundersGame,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: berlin,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: crssd,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: thiryEighthGrad,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: dodgersStadium,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: diggleJTree,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: tahoeSunrise,
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jordanby19@gmail.com",
+    YOUR_FONE: "415.518.0073",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
