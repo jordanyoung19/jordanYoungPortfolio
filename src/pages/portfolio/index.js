d8927b815998ccf4b4c277dd101ee338f4aeb1f3
Jordan Young <jordanby@uci.edu>
2023-03-02 23:18:27 -0800

    fixing portolio

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -25,8 +25,8 @@ export const Portfolio = () => {
               <div key={i} className="po_item">
                 <img src={data.img} alt="" />
                 <div className="content">
-                  <p>{data.desctiption}</p>
-                  <a href={data.link}>view project</a>
+                  {/* <p>{data.desctiption}</p>
+                  <a href={data.link}>view project</a> */}
                 </div>
               </div>
             );
